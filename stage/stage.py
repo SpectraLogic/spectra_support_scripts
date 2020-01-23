d736467069d3acee04ad141149b3a898d93781a0
Ryan Moore <ryan.moore@greyrocksoft.com>
2020-01-23 13:07:56 -0700

    Minor tweeks to get it working

--- stage/stage.py
+++ stage/stage.py
@@ -11,9 +11,13 @@ def main(pool_name, bucket_name):
 
     blob_response = client.get_blobs_on_pool_spectra_s3(ds3.GetBlobsOnPoolSpectraS3Request(pool_name))
 
-    objects = [obj['Name'] for obj in blob_response.result['Objectist']
-               if obj['InCache'] == 'false'
-               and obj['Bucket'] == bucket_name]
+    ds3_objects = blob_response.result['ObjectList']
+
+    object_names = [obj['Name'] for obj in ds3_objects if obj['Length'] != '0']
+
+    print("Objects to stage {}".format(object_names))
+
+    objects = [ds3.Ds3GetObject(obj) for obj in object_names]
 
     buffer = []
 
@@ -21,8 +25,13 @@ def main(pool_name, bucket_name):
         buffer.append(objName)
 
         if len(buffer) >= 100000:
-            print("Emitting staging job with ")
-            request = ds3.StageObjectsJobSpectraS3Request(bucket_name, buffer)
-            stage_job_response = client.stage_objects_job_spectra_s3(request)
+            create_stage_job(buffer, client, bucket_name)
             buffer = []
-            print("Stage job {} created".format(stage_job_response['JobId']))
+    create_stage_job(buffer, client, bucket_name)
+
+
+def create_stage_job(buffer, client, bucket_name):
+    print("Emitting staging job")
+    request = ds3.StageObjectsJobSpectraS3Request(bucket_name, buffer)
+    stage_job_response = client.stage_objects_job_spectra_s3(request)
+    print("Stage job {} created".format(stage_job_response['JobId']))
